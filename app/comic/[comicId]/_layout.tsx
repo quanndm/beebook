ed099369a6fa801e5d72cefe6cb09d5555a69988
quanndm <minhquan1852@gmail.com>
2024-10-04 21:57:46 +0700

    feat(UI): update handle back button at comic index page

--- app/comic/[comicId]/_layout.tsx
+++ app/comic/[comicId]/_layout.tsx
@@ -1,11 +1,21 @@
-import { Stack } from 'expo-router';
+import { CustomIcon } from '@/components';
+import { router, Stack } from 'expo-router';
 import React from 'react'
 
 const ComicDetailLayout = () => {
     return (
         <>
             <Stack>
-                <Stack.Screen name="index" />
+                <Stack.Screen name="index" options={{
+                    headerLeft: (props) => (
+                        <CustomIcon
+                            name='arrow-back-outline'
+                            size={24}
+                            onPress={() => router.replace("/(tabs)/home")}
+                            className='mr-4'
+                        />
+                    ),
+                }} />
                 <Stack.Screen name="read" />
             </Stack>
         </>
